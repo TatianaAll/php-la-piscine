431390b43a314cd78796288399d6d3440681b7e1
TatianaAll <tatiana.allery@orange.fr>
2024-11-06 13:40:33 +0100

    repare the function addArticle youpi

--- controller/admin/insertArticle-controller.php
+++ controller/admin/insertArticle-controller.php
@@ -29,25 +29,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         // je récupère le chemin du fichier json qui servira à stocker les données
         $path = '../../model/articles.json';
-        if (file_exists($path)) {
-            $articles = findArticle($path);
-
-            // Si json_decode retourne null (erreur ou fichier vide), on initialise un tableau vide
-            if ($articles === null) {
-                $articles = [];
-            }
-        } else {
-            // Si le fichier n'existe pas, je créer un tableau vide
-            $articles = [];
-        }
-        // Ajouter l'article à la liste des articles
-        $article['id'] = count($articles) + 1;  // Assigner un ID unique
-        $articles[] = $article;
-
-        // je convertis mon article en json
-        $jsonString = json_encode($articles, JSON_PRETTY_PRINT);
-        // Réécrire le contenu dans le fichier JSON
-        file_put_contents($path, $jsonString);
+       addArticle($article, $path);
 
         $isArticleCreated = true;
     }
--- services/articles-services.php
+++ services/articles-services.php
@@ -1,5 +1,25 @@
 <?php
+function addArticle($article, $path){
+    if (file_exists($path)) {
+        $articles = findArticle($path);
 
+        // Si json_decode retourne null (erreur ou fichier vide), on initialise un tableau vide
+        if ($articles === null) {
+            $articles = [];
+        }
+    } else {
+        // Si le fichier n'existe pas, je créer un tableau vide
+        $articles = [];
+    }
+    // Ajouter l'article à la liste des articles
+    $article['id'] = count($articles) + 1;  // Assigner un ID unique
+    $articles[] = $article;
+
+    // je convertis mon article en json
+    $jsonString = json_encode($articles, JSON_PRETTY_PRINT);
+    // Réécrire le contenu dans le fichier JSON
+    file_put_contents($path, $jsonString);
+}
 function findArticle($path)
 {
     $jsonString = file_get_contents($path);
